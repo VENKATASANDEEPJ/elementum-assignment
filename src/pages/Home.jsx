6b616ea038cca4adee3912f7f2233172883639cc
VENKATASANDEEPJ <venkatasandeepj0987@gmail.com>
2026-06-04 23:44:51 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Tomorrow from "../components/Tomorrow/Tomorrow";
 import Progress from "../components/Progress/Progress";
 import Services from "../components/Services/Services";
 import Testimonials from "../components/Testimonials/Testimonials";
-import Newsletter from "../components/Newsletter/Newsletter";
+import Newsletter from "../components/newsletter/Newsletter";
 import Footer from "../components/Footer/Footer";
 
 function Home() {
